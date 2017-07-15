85ec0a328bf37c58f61d19b1f6b7e9a06d90caa5
Yash Shah <yash.shah7197@live.com>
2017-07-15 13:23:54 +0530

    Add user registration logic to RegisterActivity

--- app/src/main/java/io/yashshah/bunksheetmanagementsystem/RegisterActivity.java
+++ app/src/main/java/io/yashshah/bunksheetmanagementsystem/RegisterActivity.java
@@ -1,17 +1,33 @@
 package io.yashshah.bunksheetmanagementsystem;
 
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,11 +39,17 @@ public class RegisterActivity extends AppCompatActivity {
 
     private Button mRegisterButton;
 
+    private ProgressDialog mProgressDialog;
+
     private String mFirstName;
     private String mLastName;
     private String mEmail;
     private String mPassword;
     private String mConfirmPassword;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +58,19 @@ public class RegisterActivity extends AppCompatActivity {
 
         setupViews();
         setupTextWatchers();
+        setupFirebase();
     }
 
     private void setupViews() {
         mFirstNameInputLayout =
                 (TextInputLayout) findViewById(R.id.textInputLayout_register_firstName);
+        mFirstNameInputLayout.getEditText().setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+                | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
 
         mLastNameInputLayout =
                 (TextInputLayout) findViewById(R.id.textInputLayout_register_lastName);
+        mLastNameInputLayout.getEditText().setInputType(InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+                | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
 
         mEmailInputLayout = (TextInputLayout) findViewById(R.id.textInputLayout_register_email);
 
@@ -53,12 +80,21 @@ public class RegisterActivity extends AppCompatActivity {
         mConfirmPasswordInputLayout =
                 (TextInputLayout) findViewById(R.id.textInputLayout_register_confirmPassword);
 
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setTitle(getString(R.string.user_registration));
+        mProgressDialog.setMessage(getString(R.string.creating_user));
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setIndeterminate(true);
+
         mRegisterButton = (Button) findViewById(R.id.button_register);
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mProgressDialog.show();
                 if (!userDetailsValid()) {
                     showValidationErrorDialog();
+                } else {
+                    registerUser();
                 }
             }
         });
@@ -171,6 +207,12 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    private void setupFirebase() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mFirebaseDatabase.getReference();
+    }
+
     private boolean isValidFirstName(CharSequence firstName) {
         return (!TextUtils.isEmpty(firstName));
     }
@@ -213,6 +255,101 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
             }
         });
+        builder.setCancelable(false);
         builder.show();
+    }
+
+    private void showRegistrationSuccessfulDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.registration_successful_title));
+        builder.setMessage(getString(R.string.registration_successful_message));
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    private void showRegistrationFailedDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.registration_failed_title));
+        builder.setMessage(getString(R.string.registration_failed_message));
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+    private void updateUserDetails(final FirebaseUser firebaseUser) {
+        UserProfileChangeRequest changeRequest = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mFirstName + " " + mLastName)
+                .build();
+
+        firebaseUser.updateProfile(changeRequest)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, getString(R.string.unknown_error),
+                                    Toast.LENGTH_SHORT)
+                                    .show();
+                        } else {
+                            mDatabaseReference = mFirebaseDatabase.getReference().child("Users");
+
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("name", firebaseUser.getDisplayName());
+                            hashMap.put("year", "");
+                            hashMap.put("division", "");
+                            hashMap.put("roll_no", "");
+                            hashMap.put("teacher_guardian", "");
+                            hashMap.put("class_teacher", "");
+                            hashMap.put("bunksheets_requested", "");
+
+                            mDatabaseReference.push().setValue(hashMap);
+                        }
+                    }
+                });
+    }
+
+    private void sendVerificationEmail(FirebaseUser firebaseUser) {
+        firebaseUser.sendEmailVerification()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        mProgressDialog.dismiss();
+                        showRegistrationSuccessfulDialog();
+                    }
+                });
+    }
+
+    private void registerUser() {
+        mFirstName = mFirstNameInputLayout.getEditText().getText().toString().trim();
+        mLastName = mLastNameInputLayout.getEditText().getText().toString().trim();
+        mEmail = mEmailInputLayout.getEditText().getText().toString().trim();
+        mPassword = mPasswordInputLayout.getEditText().getText().toString().trim();
+
+        mFirebaseAuth.createUserWithEmailAndPassword(mEmail, mPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser currentUser = mFirebaseAuth.getCurrentUser();
+                            if (currentUser != null) {
+                                updateUserDetails(currentUser);
+                                sendVerificationEmail(currentUser);
+                            }
+                        } else {
+                            showRegistrationFailedDialog();
+                        }
+                    }
+                });
     }
 }
