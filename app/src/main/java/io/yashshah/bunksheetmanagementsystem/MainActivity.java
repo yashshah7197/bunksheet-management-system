2448a64cb6e756377cdfb77c066b0f089bb2f3bc
Yash Shah <yash.shah7197@live.com>
2017-07-20 22:38:43 +0530

    Add FeedbackFragment

--- app/src/main/java/io/yashshah/bunksheetmanagementsystem/MainActivity.java
+++ app/src/main/java/io/yashshah/bunksheetmanagementsystem/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +15,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -164,25 +165,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void selectNavigationItem(MenuItem item) {
+        Fragment fragment = null;
+
         switch (item.getItemId()) {
             case R.id.navigation_bunksheets:
-                Toast.makeText(MainActivity.this, R.string.bunksheets, Toast.LENGTH_SHORT)
-                        .show();
+                fragment = BunksheetsFragment.newInstance();
                 setTitle(item.getTitle());
                 mDrawerLayout.closeDrawers();
                 break;
 
             case R.id.navigation_approve_bunksheets:
-                Toast.makeText(MainActivity.this, R.string.approve_bunksheets,
-                        Toast.LENGTH_SHORT)
-                        .show();
+                fragment = ApproveBunksheetsFragment.newInstance();
                 setTitle(item.getTitle());
                 mDrawerLayout.closeDrawers();
                 break;
 
             case R.id.navigation_profile:
-                Toast.makeText(MainActivity.this, R.string.profile, Toast.LENGTH_SHORT)
-                        .show();
+                fragment = ProfileFragment.newInstance();
                 setTitle(item.getTitle());
                 mDrawerLayout.closeDrawers();
                 break;
@@ -192,11 +191,18 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.navigation_feedback:
-                Toast.makeText(MainActivity.this, R.string.feedback, Toast.LENGTH_SHORT)
-                        .show();
+                fragment = FeedbackFragment.newInstance();
                 setTitle(item.getTitle());
                 mDrawerLayout.closeDrawers();
                 break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameLayout_fragments, fragment)
+                    .commit();
         }
     }
 
