099ac51478a65c52f05cd86a89282676b2618dfb
Yash Shah <yash.shah7197@live.com>
2017-07-22 22:58:47 +0530

    Add RequestBunksheetActivity

---
+++ app/src/main/java/io/yashshah/bunksheetmanagementsystem/RequestBunksheetActivity.java
@@ -0,0 +1,13 @@
+package io.yashshah.bunksheetmanagementsystem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RequestBunksheetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_bunksheet);
+    }
+}
