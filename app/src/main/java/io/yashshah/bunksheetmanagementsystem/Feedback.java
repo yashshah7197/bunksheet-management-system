2328d2b3daf7228ecf260dd2f1dbe6d927de1846
Yash Shah <yash.shah7197@live.com>
2017-07-30 11:59:58 +0530

    Add Feedback class

---
+++ app/src/main/java/io/yashshah/bunksheetmanagementsystem/Feedback.java
@@ -0,0 +1,30 @@
+package io.yashshah.bunksheetmanagementsystem;
+
+/**
+ * Created by yashshah on 30/07/17.
+ */
+
+public class Feedback {
+
+    private String mUserExperienceRating;
+    private String mFunctionalityRating;
+    private String mOtherComments;
+
+    public Feedback(String mUserExperienceRating, String mFunctionalityRating, String mOtherComments) {
+        this.mUserExperienceRating = mUserExperienceRating;
+        this.mFunctionalityRating = mFunctionalityRating;
+        this.mOtherComments = mOtherComments;
+    }
+
+    public String getUserExperienceRating() {
+        return mUserExperienceRating;
+    }
+
+    public String getFunctionalityRating() {
+        return mFunctionalityRating;
+    }
+
+    public String getOtherComments() {
+        return mOtherComments;
+    }
+}
