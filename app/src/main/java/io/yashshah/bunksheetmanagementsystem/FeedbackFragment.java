235faaefe9032cdc34482798ba4f06d276d174a1
Yash Shah <yash.shah7197@live.com>
2017-07-30 12:28:44 +0530

    Add send feedback logic to FeedbackFragment

--- app/src/main/java/io/yashshah/bunksheetmanagementsystem/FeedbackFragment.java
+++ app/src/main/java/io/yashshah/bunksheetmanagementsystem/FeedbackFragment.java
@@ -1,17 +1,43 @@
 package io.yashshah.bunksheetmanagementsystem;
 
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FeedbackFragment extends Fragment {
+
+    private View mRootView;
+
+    private Spinner mUserExperienceRatingSpinner;
+    private Spinner mFunctionalityRatingSpinner;
+
+    private TextInputLayout mOtherCommentsInputLayout;
+
+    private Button mSendFeedbackButton;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mFeedbackDatabaseReference;
+
+    private ProgressDialog mProgressDialog;
 
     public FeedbackFragment() {
         // Required empty public constructor
@@ -30,7 +56,73 @@ public class FeedbackFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feedback, container, false);
+        mRootView = inflater.inflate(R.layout.fragment_feedback, container, false);
+        setupViews();
+        setupFirebase();
+        return mRootView;
     }
 
+    private void setupViews() {
+        mUserExperienceRatingSpinner =
+                (Spinner) mRootView.findViewById(R.id.spinner_feedback_userExperience);
+        mFunctionalityRatingSpinner =
+                (Spinner) mRootView.findViewById(R.id.spinner_feedback_functionality);
+
+        mOtherCommentsInputLayout =
+                (TextInputLayout) mRootView.findViewById(R.id.textInputLayout_otherComments);
+
+        mSendFeedbackButton = (Button) mRootView.findViewById(R.id.button_send_feedback);
+        mSendFeedbackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendFeedback();
+            }
+        });
+
+        mProgressDialog = new ProgressDialog(getActivity());
+        mProgressDialog.setTitle(getString(R.string.submitting_feedback_title));
+        mProgressDialog.setMessage(getString(R.string.submitting_feedback_message));
+        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        mProgressDialog.setIndeterminate(true);
+        mProgressDialog.setCancelable(false);
+    }
+
+    private void setupFirebase() {
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFeedbackDatabaseReference = mFirebaseDatabase.getReference().child("Feedback");
+    }
+
+    private void sendFeedback() {
+        mProgressDialog.show();
+        String userExperience = mUserExperienceRatingSpinner.getSelectedItem().toString();
+        String functionality = mFunctionalityRatingSpinner.getSelectedItem().toString();
+        String otherComments = mOtherCommentsInputLayout.getEditText().getText().toString().trim();
+
+        Feedback feedback = new Feedback(userExperience, functionality, otherComments);
+
+        mFeedbackDatabaseReference.push().setValue(feedback)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mProgressDialog.dismiss();
+                            showThankYouAlertDialog();
+                        }
+                    }
+                });
+    }
+
+    private void showThankYouAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(getString(R.string.thank_you));
+        builder.setMessage(getString(R.string.feedback_sent_successfully));
+        builder.setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setCancelable(false);
+        builder.show();
+    }
 }
